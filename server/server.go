138847a7b183ed858fa248f4ff0a3c57e1262b13
Hiroshii8 <suryantodjong@gmail.com>
2019-12-08 20:35:03 +0700

    feat(repo init) : adding precommit and refactor code naming
    new changes :
    - adding pre-commit hooks with (go lint for now)
    - adding dep
    - update README.md content

--- server/server.go
+++ server/server.go
@@ -1,20 +1,24 @@
-package Server
+package server
 
 import (
-	"github.com/Hiroshii8/GoTrans/Handler"
 	"net/http"
+
+	"github.com/Hiroshii8/GoTrans/handler"
 )
 
+// Server is struct type
 type Server struct {
 	port string
 }
 
+// Start ..
 func (s *Server) Start() error {
 	h := Handler.Handler{}
 	http.HandleFunc("/", h.TranslateHandler)
 	return http.ListenAndServe(s.port, nil)
 }
 
+// NewServer ..
 func NewServer(port string) *Server {
 	return &Server{
 		port: port,
